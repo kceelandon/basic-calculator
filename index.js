c6d0df94ec50aae94b449a7894d402da90814fbe
Kcee Landon <landonkcee@gmail.com>
2024-04-29 02:11:25 -0700

    add basic html template for calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+let displayValue
+
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
+
+function operate(numOne, operator, numTwo) {
+    if (operator === "+") {
+        return add(numOne, numTwo);
+    } else if (operator === "-") {
+        return subtract(numOne, numTwo);
+    } else if (operator === "*") {
+        return multiply(numOne, numTwo);
+    } else {
+        return divide(numOne, numTwo);
+    }
+}
