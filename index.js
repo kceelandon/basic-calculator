7b2f2b21321906588d9ad11e670d3bc9703a1798
kceelandon <landonkcee@gmail.com>
2024-04-30 15:43:01 -0700

    added more button event listeners for operators

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let displayValue = "0";
-let currentButton = "0";
+let currentOperator = null;
+let firstNum = 0;
+let secondNum = null;
 
 function add(numOne, numTwo) {
     return numOne + numTwo;
@@ -25,13 +27,17 @@ function operate(numOne, operator, numTwo) {
     } else if (operator === "*") {
         return multiply(numOne, numTwo);
     } else {
+        if (numTwo === 0) {
+            alert("Cannot divide by zero.");
+            return null;
+        }
         return divide(numOne, numTwo);
     }
 }
 
 // initializes number and clear button functionality to populate display.
-function initializeButtons() {
-    let buttons = document.querySelectorAll(".button-column button");
+function initializeNumButtons() {
+    let buttons = document.querySelectorAll(".button-column button, #zero");
     buttons.forEach((btn) => {
         btn.addEventListener("click", function() {
             if (displayValue === "0") {
@@ -44,8 +50,47 @@ function initializeButtons() {
     });
     document.getElementById("clear").addEventListener("click", function() {
         displayValue = "0";
+        currentOperator = null;
         document.getElementById("display-content").textContent = displayValue;
     });
 }
 
-initializeButtons();
+function initializeOperators() {
+    initializeAdd();
+    initializeSubtract();
+    initializeMultiply();
+    initializeDivide();
+}
+
+function initializeAdd() {
+    document.getElementById("add").addEventListener("click", function() {
+        if (firstNum !== null) {
+            currentOperator = "+";
+        }
+    });
+}
+
+function initializeSubtract() {
+    document.getElementById("subtract").addEventListener("click", function () {
+        if (firstNum !== null) {
+            currentOperator = "-";
+        }
+    });
+}
+
+function initializeMultiply() {
+    document.getElementById("multiply").addEventListener("click", function () {
+        if (firstNum !== null) {
+            currentOperator = "*";
+        }
+    });
+}
+
+function initializeDivide() {
+    document.getElementById("divide").addEventListener("click", function () {
+        if (firstNum !== null) {
+            currentOperator = "/";
+        }
+    });
+}
+initializeNumButtons();
